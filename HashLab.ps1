65dd3dc58c6803d183e456b8da9f2190fab9293c
Lessflrs <126152753+Lessflrs@users.noreply.github.com>
2023-05-04 15:37:19 -0600

    Create HashLab.ps1

---
+++ HashLab.ps1
@@ -0,0 +1,32 @@
+function Drivers-uno
+{
+param( 
+	[string]$TargetFolder="C:/Windows/System32/drivers/",
+	[string]$ResultFile="Drivers.txt"
+)
+
+Get-Childitem $TargetFolder -Recurse -Include *.dll | Get-FileHash | Select-Object -Property Hash, Path | Format-Table -HideTableHeaders | Out-File $ResultFile -Encoding utf8 
+}
+
+
+function Drivers-dos
+{
+param( 
+	[string]$TargetFolder="C:/Windows/System32/drivers/",
+	[string]$ResultFile="Drivers2.txt"
+)
+
+Get-Childitem $TargetFolder -Recurse -Include *.dll | Get-FileHash | Select-Object -Property Hash, Path | Format-Table -HideTableHeaders | Out-File $ResultFile -Encoding utf8 
+}
+
+Drivers-uno
+
+$pepe = Test-Path -path "C:\Users\less_\Desktop\pia\Drivers2.txt" -PathType Leaf
+
+if($pepe -eq $True ) 
+{
+}
+else
+{
+Drivers-dos
+}
